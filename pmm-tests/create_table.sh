6eb71dbb0d6da402201258aaae8b62a7ff7f4fb5
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2017-03-10 15:04:56 +0400

    Removing function call from create_table.sh

--- pmm-tests/create_table.sh
+++ pmm-tests/create_table.sh
@@ -2,30 +2,23 @@ CLIENT_NAME=$1
 WORKDIR="${PWD}"
 MYSQL_USER=root
 
-function add_tables() {
-  #mkdir -p $WORKDIR/logs
-  #for i in ${ADDCLIENT[@]};do
-    #CLIENT_NAME=$(echo $i | grep -o  '[[:alpha:]]*')
-    if [[ "${CLIENT_NAME}" == "ps" ]]; then
-      BASEDIR=$(ls -1td ?ercona-?erver-5.* | grep -v ".tar" | head -n1)
-      BASEDIR="$WORKDIR/$BASEDIR"
 
-    elif [[ "${CLIENT_NAME}" == "ms" ]]; then
-      BASEDIR=$(ls -1td mysql-5.* | grep -v ".tar" | head -n1)
-      BASEDIR="$WORKDIR/$BASEDIR"
+if [[ "${CLIENT_NAME}" == "ps" ]]; then
+  BASEDIR=$(ls -1td ?ercona-?erver-5.* | grep -v ".tar" | head -n1)
+  BASEDIR="$WORKDIR/$BASEDIR"
 
-    elif [[ "${CLIENT_NAME}" == "pxc" ]]; then
-      BASEDIR=$(ls -1td Percona-XtraDB-Cluster-5.* | grep -v ".tar" | head -n1)
-      BASEDIR="$WORKDIR/$BASEDIR"
-    fi
+elif [[ "${CLIENT_NAME}" == "ms" ]]; then
+  BASEDIR=$(ls -1td mysql-5.* | grep -v ".tar" | head -n1)
+  BASEDIR="$WORKDIR/$BASEDIR"
 
-    for i in $(sudo pmm-admin list | grep "mysql:metrics" | sed 's|.*(||;s|)||') ; do
-      let COUNTER=COUNTER+1
-			MYSQL_SOCK=${i}
-      for num in $(seq 1 1 100)
-			    ${BASEDIR}/bin/mysql --user=${MYSQL_USER} --socket=${MYSQL_SOCK} -e "create table t${num}(id int not null)"
-      done
-		done
-}
+elif [[ "${CLIENT_NAME}" == "pxc" ]]; then
+  BASEDIR=$(ls -1td Percona-XtraDB-Cluster-5.* | grep -v ".tar" | head -n1)
+  BASEDIR="$WORKDIR/$BASEDIR"
+fi
 
-add_tables
+for i in $(sudo pmm-admin list | grep "mysql:metrics" | sed 's|.*(||;s|)||') ; do
+	MYSQL_SOCK=${i}
+  for num in $(seq 1 1 100)
+	    ${BASEDIR}/bin/mysql --user=${MYSQL_USER} --socket=${MYSQL_SOCK} -e "create table t${num}(id int not null)"
+  done
+done
