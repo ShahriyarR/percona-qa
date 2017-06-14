6a2e9cdf36f0838f5b1a4dde69f71f0241ed5778
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2017-06-14 14:26:37 +0400

    Added function for getting socket file from ps of pmm

--- pmm-tests/randomized_instances.py
+++ pmm-tests/randomized_instances.py
@@ -1,5 +1,5 @@
 import threading
-import subprocess
+from subprocess import Popen, PIPE
 import shlex
 import os
 
@@ -18,4 +18,24 @@ def call_pmm_framework(i_name, i_count):
                     stdout=None,
                     stderr=None)
 
-call_pmm_framework("ps",2)
+#call_pmm_framework("ps",2)
+
+def getting_instance_socket():
+    # For obtaining socket file path for each added instances
+    command = "sudo pmm-admin list | grep "mysql:metrics" | sed 's|.*(||;s|)||'"
+    new_command = shlex.split(command)
+    process = Popen(shlex.split(cmd), stdout=PIPE)
+    process.communicate()
+
+getting_instance_socket()
+
+def adding_instances(i_count):
+    """
+    Will try to add instances with randomized names based on already added instances
+    """
+    # This is a multi-threaded run
+    
+    workers = [threading.Thread(target=backup_obj.run_all(backup_dir="thread_"+str(i)), name="thread_"+str(i))
+                   for i in range(i_count)]
+    [worker.start() for worker in workers]
+    [worker.join() for worker in workers]
