f99ce326ad90b0b68e811827fd6718feae795580
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2017-06-14 14:08:40 +0400

    Fixing path issue

--- pmm-tests/randomized_instances.py
+++ pmm-tests/randomized_instances.py
@@ -1,14 +1,17 @@
 import threading
 import subprocess
 import shlex
+import os
 
 def call_pmm_framework(i_name, i_count):
     """
     Will call pmm-framework.sh script with given instance name and count.
     """
     # Not a multi-threaded run
-    command = "pmm-framework.sh --addclient={},{}"
-    new_command = command.format(i_name, i_count)
+    abspath = os.path.abspath(__file__)
+    dname = os.path.dirname(abspath)
+    command = "{}/pmm-framework.sh --addclient={},{}"
+    new_command = command.format(dbname, i_name, i_count)
     process = subprocess.Popen(
                     shlex.split(new_command),
                     stdin=None,
