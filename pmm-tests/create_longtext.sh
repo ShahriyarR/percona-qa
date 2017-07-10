eb49a0d00e4aedf2bb0e5b00f77a3080008fc4ef
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2017-07-10 14:38:08 +0400

    Changing random caller

--- pmm-tests/create_longtext.sh
+++ pmm-tests/create_longtext.sh
@@ -21,7 +21,7 @@ fi
 
 # Function for randomg string
 function random-string() {
-     head -n 1 < /usr/bin/cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w ${1:-32} | head -n 1
+     /usr/bin/cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w ${1:-32} | head -n 1
 }
 
 str=$(random-string ${STRING_LENGTH})
