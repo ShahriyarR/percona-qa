526268d058071250f8cd619cf9b0663cf80060bc
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2017-07-10 15:01:49 +0400

    Yet another fix

--- pmm-tests/pmm_stress_test_py/randomized_instances.py
+++ pmm-tests/pmm_stress_test_py/randomized_instances.py
@@ -270,14 +270,14 @@ def insert_blob(insert_count, i_type):
     else:
         return 0
 
-def insert_longtext(insert_count, string_length, i_type):
+def insert_longtext(i_type, insert_count, string_length):
     """
     Function to generate string length up to given number, as well as run insert statements up to given number.
     Using create_longtext.sh script here.
     """
     abspath = os.path.abspath(__file__)
     dname = os.path.dirname(abspath)
-    bash_command = '{}/create_longtext.sh {} {}'
+    bash_command = '{}/create_longtext.sh {} {} {}'
     new_command = bash_command.format(dname[:-18], i_type, insert_count, string_length)
     try:
         process = Popen(
@@ -392,7 +392,9 @@ def run_all(threads, instance_type,
             insert_blob(insert_blobs, instance_type)
         if insert_longtexts:
             print(insert_longtexts)
-            insert_longtext(insert_longtexts[0], insert_longtexts[1], instance_type)
+            print(insert_longtexts[0])
+            print(insert_longtexts[1])
+            insert_longtext(instance_type, insert_longtexts[0], insert_longtexts[1])
 
 
 if __name__ == "__main__":
